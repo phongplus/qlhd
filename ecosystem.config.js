525085cf5712355d31561c845d0b45bcaa4eb898
NguyenHoangHon <nhhon110100@gmail.com>
2022-10-29 15:59:36 +0700

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,19 @@
 module.exports = {
   apps : [{
-    name: 'qlhd_hnn',
-    append_env_to_name: true,
-    script: './server/index.js',
-    instances: 2,
-    autorestart: true,
-    watch: '.',
-  },
-    env: {
-       NODE_ENV: 'development'
-  },
-    env_production:{
-	NODE_ENV: "production",
-	PORT: 5000
-    },
-    env_development: {
-	NODE_ENV: "development",
-	PORT: 5000
-    },
-]],
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
 
   deploy : {
     production : {
-      user : 'admin_hh',
-      host : '103.88.121.45',
-      ref  : 'origin/main',
-      repo : 'https://gitlab.honeynet.vn/outsourcing-projects/qlhd_hnn',
-      path : '~/admin_hh/qlhd_hnn',
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
