daa40fbf1cd0fcd93431a315557e38185c6b416d
NguyenHoangHon <nhhon110100@gmail.com>
2022-10-29 16:13:00 +0700

    deploy Node server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
+    name: 'qlhd_hnn',
+    script: './server/index.js',
+    cwd: __dirname,
+    instances: 2,
+    autorestart: true,
+    exec_mode: 'cluster',
+    watch: true,
+    env:{
+      NODE_ENV:'production'
+    }
   }, {
     script: './service-worker/',
     watch: ['./service-worker']
@@ -9,11 +17,11 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      user : 'nhhon110100@gmail.com',
+      host : '103.88.121.45',
+      ref  : 'origin/main',
+      repo : 'git@gitlab.honeynet.vn:63379/outsourcing-projects/qlhd_hnn.git',
+      path : 'qlhd_hnn/server',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
